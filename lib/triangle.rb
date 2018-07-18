383ce99d343455124729736ca866c5b9a56b47c2
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-18 08:19:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,13 @@ class Triangle
 
   def kind
     validate_triangle
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    elsif
+      side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+      :isosceles
+    else
+      :scalene
   end
 
 end
