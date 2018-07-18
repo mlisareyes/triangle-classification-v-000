bfd7b214bb5edcba9b93571b9b3a59f79501f2cd
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-18 08:27:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,5 +22,6 @@ class Triangle
   def valid_triangle
     valid_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
     [side_1, side_2, side_3].each {|s| valid_triangle << false if s <=0}
+    raise TriangleError if valid_triangle.include?(false)
   end
 end
