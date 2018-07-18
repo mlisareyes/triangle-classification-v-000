43a78ed7ac4dd1711f9defe4b6e77d7c80352a00
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-18 08:11:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,13 @@
 class Triangle
   # write code here
-  attr_accessor :x, :y, :z
+  attr_accessor :side_1, :side_2, :side_3
 
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
+
+  def kind
+
 end
